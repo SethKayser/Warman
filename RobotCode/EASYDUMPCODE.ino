626a7d465e076ad0db23b4bfd4400987b059e258
LochnessMobstr <lachlan.new8@gmail.com>
2026-05-28 13:38:39 +1000

    Update EASYDUMPCODE.ino

--- RobotCode/EASYDUMPCODE.ino
+++ RobotCode/EASYDUMPCODE.ino
@@ -91,12 +91,12 @@ constexpr unsigned long EXTENSION_ENABLE_SETTLE_MS = 50;
 // ============================================================
 
 // ----- Pitch movements -----
-constexpr unsigned long PITCH_DOWN_STEPS       = 380;
+constexpr unsigned long PITCH_DOWN_STEPS       = 400;
 constexpr unsigned long PITCH_UP_STEPS_ONE     = 1200;
 constexpr unsigned long PITCH_UP_STEPS_TWO     = 1100;
 
 // ----- Extension movements -----
-constexpr unsigned long EXTENSION_START_STEPS           = 770;
+constexpr unsigned long EXTENSION_START_STEPS           = 900;
 constexpr unsigned long EXTENSION_EXTRA_STEPS           = 700;
 constexpr unsigned long EXTENSION_FINAL_STEPS           = 500;
 constexpr unsigned long EXTENSION_AFTER_SERVO_OFF_STEPS = 700;
@@ -104,7 +104,7 @@ constexpr unsigned long EXTENSION_TO_MAX_STEPS          = 950;
 constexpr unsigned long EXTENSION_BACK                  = 2700;
 
 // ----- Rotate movement -----
-constexpr unsigned long ROTATE_LEFT_STEPS = 450;
+constexpr unsigned long ROTATE_LEFT_STEPS = 420;
 
 // ============================================================
 // SERVO SETTINGS
